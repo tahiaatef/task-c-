f87a8cc4c86f4d253f1269e733871140107be548
tya <99808858+tahiaatef@users.noreply.github.com>
2022-12-17 17:57:55 +0200

    add personclass

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace task;
+    class  abstract person{
+       public string name;
+       public int age;
+      public person ( string _name , int _age){
+        name = _name;
+        age = _age;
+       }
+       public abstract void print(){
+
+        }
+        }
