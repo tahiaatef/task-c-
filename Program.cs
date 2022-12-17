d70f54b29e68745e9c69700adba47a5c4ddc60b5
tya <99808858+tahiaatef@users.noreply.github.com>
2022-12-17 18:41:43 +0200

    class student

--- Program.cs
+++ Program.cs
@@ -10,4 +10,15 @@ namespace task;
        public abstract void print(){
 
         }
+    }
+        class student : person{
+        public int year;
+        public float gpa;
+        public student( string _name , int _age , int _year , float _gpa) : base(_name , _age){
+            year = _year;
+            gpa = _gpa;
+        }
+        public override void print(){
+            Console.WriteLine("My name is " + name + "My age is " + age +"and my gpa is " + gpa);
+        }
         }
