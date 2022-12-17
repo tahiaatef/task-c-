bbf8f2e723a26d36731ecc446b244972d69dc203
tya <99808858+tahiaatef@users.noreply.github.com>
2022-12-17 18:59:14 +0200

    database staff

--- Program.cs
+++ Program.cs
@@ -22,3 +22,26 @@ namespace task;
             Console.WriteLine("My name is " + name + "My age is " + age +"and my gpa is " + gpa);
         }
         }
+        public class database{
+        private int _currentIndex;
+        public person[] people = new person[50];
+
+        public void addstudent(student stu){
+            people[_currentIndex++] = stu;
+        }
+        public void addstaff(staff staf){
+            people[_currentIndex++] = staf;
+        }
+
+      }
+    class staff : person{
+        public double salary;
+        public int joinyear;
+        public staff(double _salary , int _joinyear) : base(_name , _age){
+            salary = _salary;
+            joinyear = _joinyear;
+        }
+        public override void print(){
+            Console.WriteLine("My name is " + name + "My age is " + age +"and my salary is " + salary);
+        }
+    }
